1ee29f8c6d06856a80247c503e25df851a760b41
jisoo-youn <yjs930915@gmail.com>
2019-11-20 07:48:00 +0900

    project init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column">
+        <Card/>
+      </div>
+      <div class="column">
+        <Card/>
+      </div>
+      <div class="column">
+        <Card/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: { Card },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .container {
+  }
+
+</style>
